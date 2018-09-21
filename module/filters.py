bcedcd07159662cd6eb7226e84a24492ff5ab54c
jameslee5656 <jameslee56566@gmail.com>
2018-09-21 19:39:33 +0800

    Changed 2018-9-20\nPicture processing alterd application

---
+++ module/filters.py
@@ -0,0 +1,64 @@
+import cv2 
+import numpy as np
+import utils
+
+def stokeEdges(src, dst, blurKsize = 7, edgeKsize = 5):
+    if blurKsize >= 3:
+        blurredSrc = cv2.medianBlur(src, BlurKsize)
+        graySrc = cv2.cvtColor(blurredSrc, cv2.COLOR_BGR2GRAY)
+    else :
+        graySrc = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    cv2.Laplacian(graySrc, cv2.CV_8U, graySrc, ksize = edgeKsize)
+    normalizedInverseAlpha = (1.0 / 255) * (255 - graySrc)
+    channels = cv2.split(src)
+    for channel in channels:
+        channel[:] = channel * normalizedInverseAlpha
+    cv2.merge(channels, dst)
+
+class VConvolutionFilter(object):
+    """A filter that applies a convolution to V (or all of BGR)."""
+    
+    def __init__(self, kernel):
+        self._kernel = kernel
+        
+    def apply(self, src, dst):
+        """Apply the filter with a BGR or Gray source/destination."""
+        cv2.filter2D(src, -1, self._kernel, dst)
+        
+class Sharpenfilter(VConvolutionFilter):
+    """A sharpen filter with 1-pixel radius."""
+
+    def __init__(self):
+        kernel = np.array([[-1, -1, -1]
+                           [-1,  9, -1],
+                           [-1, -1, -1]])
+        VConvolutionFilter.__init__(self, kernel)
+
+class FindEdgesFilter(VConvolutionFilter):
+    """An edge-finding filter with a 1-pixel radius."""
+
+    def __init__(self):
+        kernel = np.array([[-1, -1, -1],
+                           [-1,  9, -1],
+                           [-1, -1, -1]])
+        VConvolutionFilter.__init__(self, kernel)
+
+class BlurFilter(VConvolutionFilter):
+    """A blur filter with a 2-pixel radius."""
+    def __init__(self):
+        kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
+                           [0.04, 0.04, 0.04, 0.04, 0.04],
+                           [0.04, 0.04, 0.04, 0.04, 0.04],
+                           [0.04, 0.04, 0.04, 0.04, 0.04],
+                           [0.04, 0.04, 0.04, 0.04, 0.04]])
+        VConvolutionFilter.__init__(self, kernel)
+
+class EmbossFilter(VConvolutionFilter):
+    """An emboss filter with a 1-pixel radius.
+       Both has blur and sharpen which produce a embossed(浮雕)"""
+    
+    def __init__(self):
+        kernel = np.array([[-2, -1, 0],
+                           [-1,  1, 1],
+                           [ 0,  1, 2]])
+        VConvolutionFilter._init(self, kernel)
--- module/manager.py
+++ module/manager.py
@@ -2,6 +2,32 @@ import cv2
 import numpy as np
 import time
 
+class WindowManager(object):
+    
+    def __init__(self, windowName, keypressCallback = None):
+        self.keypressCallback = keypressCallback
+        
+        self._windowName = windowName
+        self._isWindowCreated = False
+        
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+    def createWindow(self):
+        cv2.nameWindow(self._windowName)
+        self._isWindowCreated = True
+        
+    def show(sekf, frame):
+        cv2.imshow(self.windowName)
+        self._isWindowCreated = False
+        
+    def processEvents(self):
+        keycode = cv2.waitkey(1)
+        if self.keypressCallback is not None and keynote != -1:
+            #Discard any non-Ascii info encoded by GTK.
+            keycode &= 0xFF
+            self.keypressCallback(keycode)
+
 class CaptureManager(object):
     
     def __init__(self, capture, previewWindowManager = None, shouldMirrorPreview = False):
